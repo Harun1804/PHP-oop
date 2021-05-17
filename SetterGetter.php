f3474d60f1cb37a7a466b4f1175da7a5a2787275
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-05-17 14:32:24 +0700

    Setter Getter

--- SetterGetter.php
+++ SetterGetter.php
@@ -4,12 +4,11 @@
 class Produk {
     //visibility = public,protected, private
     //property
-    public  $judul,
+    private  $judul,
             $penulis,
-            $penerbit;
-    
-    protected $diskon = 0;
-    private $harga;
+            $penerbit,
+            $harga,
+            $diskon = 0;
     
     public function __construct($judul ="judul",$penulis="penulis",$penerbit="penerbit",$harga=0)
     {
@@ -20,6 +19,57 @@ class Produk {
     }
     
     //method
+    // setter & getter
+    public function getJudul()
+    {
+        return $this->judul;
+    }
+
+    public function setJudul($judul)
+    {
+        $this->judul = $judul;
+    }
+
+    public function getPenulis()
+    {
+        return $this->penulis;
+    }
+
+    public function setPenulis($penulis)
+    {
+        $this->penulis = $penulis;
+    }
+
+    public function getPenerbit()
+    {
+        return $this->penerbit;
+    }
+
+    public function setPenerbit($penerbit)
+    {
+        $this->penerbit = $penerbit;
+    }
+    
+    public function setHarga($harga)
+    {
+        $this->harga = $harga;
+    }
+
+    public function getHarga()
+    {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+
+    public function setDiskon($diskon)
+    {
+        $this->diskon = $diskon;
+    }
+
+    public function getDiskon()
+    {
+        return $this->diskon;
+    }
+
     public function getLabel()
     {
         return "$this->penerbit, $this->penulis";
@@ -29,11 +79,6 @@ class Produk {
     {
         $str = "{$this->judul} | {$this->getLabel()} (Rp.{$this->harga})";
         return $str;
-    }
-
-    public function getHarga()
-    {
-        return $this->harga - ($this->harga * $this->diskon / 100);
     }
 }
 
@@ -69,11 +114,6 @@ class Game extends Produk {
         $str = "Game : ". parent::getInfoProduk(). " ~ {$this->waktuMain} Jam.";
         return $str;
     }
-
-    public function setDiskon($diskon)
-    {
-        $this->diskon = $diskon;
-    }
 }
 
 class CetakInfoProduk {
@@ -97,3 +137,7 @@ echo "<hr>";
 
 $produk2->setDiskon(50);
 echo $produk2->getHarga();
+echo "<hr>";
+
+$produk1->setJudul(1);
+echo $produk1->getJudul();
