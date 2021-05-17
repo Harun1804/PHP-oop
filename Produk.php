f5783b8e90270cda1715844e4ae1e38a5b57e1c4
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-05-17 12:29:16 +0700

    Property & Method

--- Produk.php
+++ Produk.php
@@ -1,8 +1,21 @@
 <?php
 
 class Produk {
-
+    public  $judul = "judul",
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga = 0;
+    
+    public function getLabel()
+    {
+        return "$this->penulis,$this->penerbit";
+    }
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+$produk3 = new Produk();
+$produk3->judul = "One Piece";
+$produk3->penulis = "Oda";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+echo $produk3->getLabel();
